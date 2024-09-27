9615f30ec608d6f397fb0903e5cd8ef872dc36da
Huleji Tukura <61782821+kinghendrix10@users.noreply.github.com>
2024-09-27 09:49:08 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,20 @@
+# app/__init__.py
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///analysis.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    CORS(app)
+    db.init_app(app)
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
