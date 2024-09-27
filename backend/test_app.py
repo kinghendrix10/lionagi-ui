dc33999a4b88efffc07c95c7a25a80c3954c9095
Huleji Tukura <61782821+kinghendrix10@users.noreply.github.com>
2024-09-27 10:09:36 -0500

    Update test_app.py

--- backend/test_app.py
+++ backend/test_app.py
@@ -8,10 +8,14 @@ from app.models import Project, Agent, Analysis, APIService
 def client():
     app = create_app('testing')
     app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    
     with app.test_client() as client:
         with app.app_context():
             db.create_all()
         yield client
+    
+    db.session.remove()
     db.drop_all()
 
 def test_create_project(client):
